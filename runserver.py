89c5c2f9f14f0224dccfe3603af8a624f5ecfed8
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-03-04 17:44:51 +0000

    Initial commit

---
+++ runserver.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import os.path as osp
+
+import flask
+import yaml
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    root_dir = osp.abspath('logs')
+    log_dirs = os.listdir(root_dir)
+
+    app.config['STATIC_FOLDER'] = root_dir
+
+    params_keys = set()
+    params_data = {}
+    for log_dir in log_dirs:
+        params_file = osp.join(root_dir, log_dir, 'params.yaml')
+        with open(params_file) as f:
+            params = yaml.load(f)
+        params_keys = set(params.keys()) | params_keys
+        params_data[log_dir] = params
+
+    return flask.render_template(
+        'index.html',
+        root_dir=root_dir,
+        log_dirs=log_dirs,
+        params_keys=params_keys,
+        params_data=params_data,
+    )
+
+
+@app.route('/logs/<log_dir>/<path:filename>')
+def logs(log_dir, filename):
+    root_dir = osp.abspath('logs')
+    return flask.send_from_directory(osp.join(root_dir, log_dir), filename)
+
+
+app.run(debug=True)
