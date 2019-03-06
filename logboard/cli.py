77113d63903fb1b516b186aced77e43ee199c8bc
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-03-06 01:37:51 +0000

    Add --host, --port

--- logboard/cli.py
+++ logboard/cli.py
@@ -1,5 +1,14 @@
+import argparse
+
 from .views import app
 
 
 def main():
-    app.run(debug=True)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument('--host', default='localhost', help='host')
+    parser.add_argument('--port', default='5000', help='port')
+    args = parser.parse_args()
+
+    app.run(host=args.host, port=args.port, debug=True)
