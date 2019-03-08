7d85f8953b7be95f41cb249dd42b1bbaae8bec6a
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-03-08 23:08:32 +0000

    Update chainerlg/cli.py

--- logboard/cli.py
+++ logboard/cli.py
@@ -7,7 +7,7 @@ def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument('--host', default='localhost', help='host')
+    parser.add_argument('--host', default='0.0.0.0', help='host')
     parser.add_argument('--port', default='5000', help='port')
     parser.add_argument('--debug', action='store_true', help='debug mode')
     args = parser.parse_args()
