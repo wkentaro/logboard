a73e27f2765821dd67a7724d154a3a4685b9e76a
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-04-08 03:10:41 +0100

    0.2.0

--- setup.py
+++ setup.py
@@ -1,27 +1,48 @@
 from __future__ import print_function
 
+import distutils
 import shlex
 import subprocess
+import sys
 
 from setuptools import find_packages
 from setuptools import setup
 
 
-def githash():
-    cmd = 'git log -1 --format="%h"'
+version = '0.2.0'
+
+
+if sys.argv[1] == 'release':
+    if not distutils.spawn.find_executable('twine'):
+        print(
+            'Please install twine:\n\n\tpip install twine\n',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    commands = [
+        'git pull origin master',
+        'git tag v{:s}'.format(version),
+        'git push origin master --tag',
+        'python setup.py sdist',
+        'twine upload dist/logboard-{:s}.tar.gz'.format(version),
+    ]
+    for cmd in commands:
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
+
+def get_long_description():
+    with open('README.md') as f:
+        long_description = f.read()
+
     try:
-        hash_ = subprocess.check_output(shlex.split(cmd)).decode().strip()
-    except subprocess.CalledProcessError:
-        hash_ = None
-    return hash_
-
-
-version = '0.1.0-0'
-
-
-hash_ = githash()
-if hash_ is not None:
-    version = '%s.%s' % (version, hash_)
+        import github2pypi
+        return github2pypi.replace_url(
+            slug='wkentaro/logboard', content=long_description
+        )
+    except Exception:
+        return long_description
 
 
 setup(
@@ -33,6 +54,8 @@ setup(
     author='Kentaro Wada',
     author_email='www.kentaro.wada@gmail.com',
     description='Monitor and Compare Logs on Browser.',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
     url='http://github.com/wkentaro/logboard',
     license='MIT',
     classifiers=[
