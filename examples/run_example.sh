8f2ba13e12ebccddcf140222f0d861a5359d7657
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-04-08 03:06:49 +0100

    Update examples/run_example.sh

--- examples/run_example.sh
+++ examples/run_example.sh
@@ -1,3 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash
 
+echo_bold () {
+  echo -e "\033[1m$*\033[0m"
+}
+
+echo_bold "==> Running Terminal Example (logtable)"
+logtable --logdir logs
+
+echo_bold "==> Running Browser Example (logbrowser)"
 logboard --logdir logs
